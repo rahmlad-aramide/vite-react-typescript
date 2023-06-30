9c3ac699de82fe36879ffcb27b27c0f32b4795b0
rahmlad-aramide <67334984+rahmlad-aramide@users.noreply.github.com>
2023-06-30 13:55:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-react-typescript
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-n8pe55)
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+import SignInForm from './SignInForm';
+
+function App() {
+
+  return (
+    <>
+      <SignInForm />
+    </>
+  )
+}
+
+export default App
